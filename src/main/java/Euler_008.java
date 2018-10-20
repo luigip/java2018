d8008c1f17e58af1228466c89cc9e83070280a50
Rhys <rjones.zx@gmail.com>
2018-10-20 18:08:09 +0100

    used Java 8 Files.lines

--- src/main/java/Euler_008.java
+++ src/main/java/Euler_008.java
@@ -1,39 +1,30 @@
-import java.io.File;
-import java.io.FileReader;
-import java.net.URL;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 public class Euler_008 extends Problem {
 /*
-
 The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
-[see file]
+[see file in /data]
 Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 */
     int numberOfDigits = 13;
 
     long solve() {
-
-        String x = "";
-        try (FileReader fr = new FileReader("Euler_008_data.txt")){
-            StringBuilder sb = new StringBuilder();
-            while(sc.hasNext()) {
-                sb.append(sc.nextLine());
-            }
-            x = sb.toString();
-            System.out.println(sb.toString());
-        }
-        catch (Exception e) {e.printStackTrace();}
-
         long maxProduct = 0;
-        char[] digits = x.toCharArray();
-        for (int i = 0; i <= x.length() - numberOfDigits; i++) {
-            long product = 1;
-            for (int j = 0; j < numberOfDigits; j++) {
-                product = product * Character.getNumericValue(digits[i+j]);
+        try {
+            String x = Files.lines(Paths.get("data/Euler_008_data.txt")).collect(Collectors.joining());
+            char[] digits = x.toCharArray();
+            // loop over digits in input
+            for (int i = 0; i <= x.length() - numberOfDigits; i++) {
+                long product = 1;
+                // loop over substring
+                for (int j = 0; j < numberOfDigits; j++) {
+                    product = product * Character.getNumericValue(digits[i+j]);
+                }
+                if(product > maxProduct) maxProduct = product;
             }
-            if(product > maxProduct) maxProduct = product;
-        }
+        } catch (Exception e) {e.printStackTrace();}
         return maxProduct;
     }
 
