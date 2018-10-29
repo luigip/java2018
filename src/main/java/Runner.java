0445a58933ddfbd58119291ed1002d60c4362ea7
Rhys <rjones.zx@gmail.com>
2018-10-29 01:46:25 +0000

    New Euler_015 method involving nodes

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -11,7 +11,8 @@ public class Runner {
         String problemName = args[0];
         try {
             Problem problem = (Problem) Class.forName(problemName).getConstructor().newInstance();
-            System.out.println(problemName + ": " + problem.solve());
+            long result = timed(problem);
+            System.out.println(problemName + ": " + result);
         }
         catch (RuntimeException r) {
             System.out.println("Boom, runtime exception!");
@@ -26,4 +27,26 @@ public class Runner {
             e.printStackTrace();
         }
     }
+
+    static long timed(Problem p) throws IOException {
+        long t0 = System.nanoTime();
+        long result = p.solve();
+        long t1 = System.nanoTime();
+        long time = t1 - t0;
+        System.out.println("Time: "+time+" ns  =  "+time/1_000_000+" ms");
+        return result;
+    }
+
 }
+/*
+
+class Timer {
+    long timed(Problem p) throws IOException {
+        long t0 = System.nanoTime();
+        long result = p.solve();
+        long t1 = System.nanoTime();
+        long time = t1 - t0;
+        System.out.println("Time: "+time+" ns  =  "+time/1000+" ms");
+        return result;
+    }
+}*/
--- src/main/scala/Euler_015.scala
+++ src/main/scala/Euler_015.scala
@@ -5,13 +5,11 @@ there are exactly 6 routes to the bottom right corner.
 How many such routes are there through a 20×20 grid? */
 
 class Euler_015 extends Problem {
-  def solve: Long = {
-    val gridsize = 20
+  def solve1: Long = {
     // Start at bottom right. Work back, assigning the number of ways of getting to the end to each intersection.
     // This could be solved more easily by a simple Pascal's Triangle calculation, but this is the Scala way to model
     // the problem
-
-    //TODO: work out how to do this with Nodes in the case class definition rather than Ints
+    val gridsize = 20
     sealed trait Intersection {
       val ways: Long
     }
@@ -27,7 +25,6 @@ class Euler_015 extends Problem {
         if (down == Edge && across == Edge) 1 // Endpoint
         else across.ways + down.ways
     }
-
     object Node {
       private val cache = mutable.Map[(Int, Int), Node]()
       def apply(x: Int, y: Int): Node = cache.getOrElseUpdate((x,y), new Node(x,y))
@@ -35,4 +32,38 @@ class Euler_015 extends Problem {
 
     Node(gridsize, gridsize).ways
   }
+
+  def solve: Long = {
+    // A more convoluted but purer way without using co-ordinates or a mutable cache
+    // Effectively we have a 2D analogue of a LinkedList, stitched together by growing in two directions
+    // Amazingly, this worked first time. It's also 20x faster!
+    val gridsize = 20
+    sealed trait Intersection {
+      val ways: Long
+    }
+    case object Edge extends Intersection {
+      val ways = 0
+    }
+    case class Node(across: Intersection, down: Intersection) extends Intersection {
+      val ways: Long = across.ways + down.ways
+    }
+    object Zero extends Node(Edge, Edge) {
+      override val ways = 1
+    }
+    def growLeft(corner: Intersection): Intersection = corner match {
+      case Edge => Edge
+      case Node(_, down) => Node(corner, growLeft(down))
+    }
+    def growUp(corner: Intersection): Intersection = corner match {
+      case Edge => Edge
+      case Node(across, _) => Node(growUp(across), corner)
+    }
+    def grid(rowsToAdd: Int, corner: Intersection): Intersection = rowsToAdd match {
+      case 0 => corner
+      case n => grid(n - 1, Node(growUp(corner), growLeft(corner)))
+    }
+    grid(gridsize, Zero).ways
+  }
+
+
 }
