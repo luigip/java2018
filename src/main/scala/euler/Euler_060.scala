2647d4a5c7c4bc34fa80974c3eea15ff08a0768d
Rhys <rjones.zx@gmail.com>
2018-11-09 16:53:13 +0000

    Problems with arity 5

--- src/main/scala/euler/Euler_060.scala
+++ src/main/scala/euler/Euler_060.scala
@@ -17,42 +17,52 @@ import euler.common.BitSetIterator
 
 // Strategy:
 // As any two primes concatenate, any set of four within the set of five is also a 'remarkable' set
-// So look for sets of five by adding another prime to sets of four
+// So look for sets of five by adding another prime to sets of four etc
 // Similarly, get sets of four from sets of three, from sets of two
 
 class Euler_060 extends Problem {
-  private val numberOfPrimesInSet = 5
-  private val maxSearch = 9999 // this is enforced if we're checking for primes of Ints from our sieve, as Int.MaxValue
+  private val numberOfPrimesInSet = 3
+  private val maxSearch = 999 // this is enforced if we're checking for primes of Ints from our sieve, as Int.MaxValue
                                // is 9 digits, and we need to check check for concatenations
   implicit private val primes: util.BitSet = Primes.primesFromSieve(1e8.toInt) // covering all 8 digit numbers
 
   def solve: Long = {
-    getCombos(numberOfPrimesInSet, maxSearch).map(_.sum).max
+    val cs =
+      getCombos(numberOfPrimesInSet, maxSearch)       foreach println
+//      .map(_.sum).max
+
 //   Time: 3439744307 ns  =  3440 ms
 //   euler.Euler_060: 26033
+
+    0L
   }
 
-  def getCombos(arity: Int, max: Int): Iterator[Seq[Int]] = arity match {
-    case 2 => for {
-      p <- BitSetIterator from 0 to max
-      q <- BitSetIterator from (p + 1) to max
-      if isRemarkablePair(p,q)
-    } yield Seq(p,q)
+  /*private*/ lazy val pairCombos: Iterator[Set[Int]] = for {
+    p <- BitSetIterator from 0 to maxSearch
+    q <- BitSetIterator from (p + 1) to maxSearch
+    if isRemarkablePair(p,q)
+  } yield Set(p,q)
+
+  private lazy val isPairCombo = pairCombos.toSet
+
+  def getCombos(arity: Int, max: Int): Iterator[Set[Int]] = arity match {
+    case 2 => pairCombos
     case x => for {
-      remarkableSeq <- getCombos(x - 1, max)
-      p <- BitSetIterator(primes, remarkableSeq.max + 1 , max)
-      if isRemarkable(remarkableSeq, p)
-    } yield remarkableSeq :+ p
+      remarkableSet <- getCombos(x - 1, max)
+      p <- BitSetIterator from (remarkableSet.max + 1) to max
+      if isRemarkable(remarkableSet, p)
+    } yield remarkableSet + p
   }
 
   def isRemarkablePair(x: Int, y: Int) = primes.get(concatenateInts(x,y)) && primes.get(concatenateInts(y,x))
 
-  def isRemarkable(xs: Seq[Int], p: Int) = xs.forall(x => isRemarkablePair(x, p))
+  def isRemarkable(xs: Set[Int], p: Int) = xs.forall(x => isPairCombo(Set(x, p)))
 }
 
 object Test_060 extends App {
   val e = new Euler_060
-  val r = e.getCombos(2, 10)
-  assert(e.isRemarkable(Seq(3,7,109), 673))
-  println(r)
+//  val r = e.getCombos(3, 700)
+  println (e.isRemarkable(Set(3,7,109), 673))
+//  println(r)
+  e.pairCombos foreach println
 }
